ef36fb2bb0e6782c4416f1d4e2f24bffd2d78f49
Pudov Dmitry <dimapudov99@gmail.com>
2018-05-20 18:25:51 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+'use strict';
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Bauman Vizualizer Demo';
+  const options = {
+    body: 'Legen... wait for it... dary!',
+    icon: 'images/icon.png',
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://students.bmstu.ru/')
+  );
+});
